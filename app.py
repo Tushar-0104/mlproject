4632c188e44999d9b5efae81ff4a9ca5b417c807
Tushar-0104 <tusharsharma7024@gmail.com>
2024-05-03 16:18:25 -0400

    Data Transformation

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
-from src.components.data_ingestion import DataIngestion
-from src.components.data_ingestion import DataIngestionConfig
+from src.components.data_ingestion import DataIngestion,DataIngestionConfig
+# from src.components.data_ingestion import DataIngestionConfig
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
+# from src.components.data_transformation import DataTransforamtionConfig
 import sys
 
 
@@ -9,9 +11,22 @@ if __name__=="__main__":
     logging.info("The execution has started")
 
     try:
+        #data_ingestion_config=DataIngestionConfig()
         data_ingestion=DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+        #data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
+        ## Model Training
+
+        # model_trainer=ModelTrainer()
+        # print(model_trainer.initiate_model_trainer(train_arr,test_arr))
         
     except Exception as e:
-        logging.info("CustomException")
+        logging.info("Custom Exception")
         raise CustomException(e,sys)
+    
+
+
---
+++ src/mlproject/utils.py
@@ -0,0 +1,48 @@
+import os
+import pandas as pd
+import numpy as np
+from src.mlproject.logger import logging
+from src.mlproject.exception import CustomException
+import sys
+from dotenv import load_dotenv
+import pymysql
+import pickle
+
+
+load_dotenv()
+
+host=os.getenv("host")
+user=os.getenv("user")
+password=os.getenv("password")
+db=os.getenv("db")
+
+def read_sql_data(self):
+    logging.info(" Reading SQL database started")
+    try:
+         mydb=pymysql.connect(host=host,user=user,password=password,db=db)
+         logging.info("Conection establish",mydb)
+         df=pd.read_sql_query('SELECT * FROM Students',mydb)
+         print(df.head())
+ 
+         return df
+
+    except Exception as ex:
+         raise CustomException(ex)
+    
+
+def save_object(file_path,obj):
+     try:
+          dir_path=os.path.dirname(file_path)
+
+          os.makedirs(dir_path,exist_ok=True)
+
+          with open(file_path,"wb") as file_obj:
+               pickle.dump(obj,file_obj)
+
+
+     except Exception as e:
+         raise CustomException(e,sys)
+
+ 
+
+   
